06614420efc306c66ad54f4ae5a0bde0f48a14a6
Andrzej Rawski <rawski.andrzej@gmail.com>
2021-12-08 10:46:57 +0100

    Create PUT beer

--- src/main/java/pl/antygravity/microbrewery/web/controller/BeerController.java
+++ src/main/java/pl/antygravity/microbrewery/web/controller/BeerController.java
@@ -26,12 +26,20 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity<BeerDto> handlePost(BeerDto beerDto){
+    public ResponseEntity<BeerDto> handlePost(BeerDto beerDto) {
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
 
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<BeerDto> handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto) {
+
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
