a553fd59b340955acfd7c1a99d9d896aaaa7dab8
Andrzej Rawski <rawski.andrzej@gmail.com>
2021-12-18 20:16:11 +0100

    Delete Beer with @ResponseStatus

--- src/main/java/pl/antygravity/microbrewery/services/BeerServiceImpl.java
+++ src/main/java/pl/antygravity/microbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package pl.antygravity.microbrewery.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pl.antygravity.microbrewery.web.model.BeerDto;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -26,5 +28,10 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
         //todo impl - would add a real impl to update beer
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
--- src/main/java/pl/antygravity/microbrewery/web/controller/BeerController.java
+++ src/main/java/pl/antygravity/microbrewery/web/controller/BeerController.java
@@ -19,7 +19,7 @@ public class BeerController {
         this.beerService = beerService;
     }
 
-    @GetMapping("/{beerId}")
+    @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
 
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
@@ -35,11 +35,17 @@ public class BeerController {
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
-    @PutMapping("/{beerId}")
+    @PutMapping({"/{beerId}"})
     public ResponseEntity<BeerDto> handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
 
         beerService.updateBeer(beerId, beerDto);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping({"/{beerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+        beerService.deleteById(beerId);
     }
 }
