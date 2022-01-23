8c527a1eccb49cbd1719757063c63c8e9868847d
Andrzej Rawski <rawski.andrzej@gmail.com>
2022-01-23 22:05:09 +0100

    Bean Validation Implementation

--- src/main/java/pl/antygravity/microbrewery/web/controller/BeerController.java
+++ src/main/java/pl/antygravity/microbrewery/web/controller/BeerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import pl.antygravity.microbrewery.services.BeerService;
 import pl.antygravity.microbrewery.web.model.BeerDto;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @Deprecated
@@ -27,7 +28,7 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity<BeerDto> handlePost(@RequestBody BeerDto beerDto) {
+    public ResponseEntity<BeerDto> handlePost(@Valid @RequestBody BeerDto beerDto) {
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
 
         HttpHeaders headers = new HttpHeaders();
@@ -37,7 +38,7 @@ public class BeerController {
     }
 
     @PutMapping({"/{beerId}"})
-    public ResponseEntity<BeerDto> handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity<BeerDto> handleUpdate(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto) {
 
         beerService.updateBeer(beerId, beerDto);
 
